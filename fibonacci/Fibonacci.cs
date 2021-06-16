c207178098943fc64c9e441490ebe37fb4f117f1
BhanuPG <bhanubtech35@gmail.com>
2021-06-16 09:58:03 +0530

    Initial commit

---
+++ fibonacci/Fibonacci.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonacci
+{
+    public class Fibonacci
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter the number to print the Fibonacci series..");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Fibonacci obj = new Fibonacci();
+            string str = obj.getFibonacciNumbers(num);
+            Console.WriteLine(str);
+        }
+        public  string getFibonacciNumbers(int n)
+        {
+            StringBuilder str = new StringBuilder();
+            int f1 = 0, f2 = 1, i;
+
+            if (n < 1)
+                return String.Empty;
+            str.Append(f1 + " ");
+            for (i = 1; i < n; i++)
+            {
+                str.Append(f2 + " ");
+                int next = f1 + f2;
+                f1 = f2;
+                f2 = next;
+            }
+            return str.ToString().Trim();
+        }
+
+    }
+}
---
+++ fibonacciTests/FibonacciTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using fibonacci;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonacci.Tests
+{
+    [TestClass()]
+    public class FibonacciTests
+    {
+        [TestMethod()]
+        public void FibonacciWithPositiveNumber()
+        {
+            Fibonacci obj = new Fibonacci();
+            string actual= obj.getFibonacciNumbers(5);
+            string expected = "0 1 1 2 3";
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod()]
+        public void FibonacciWithNegativeNumber()
+        {
+            Fibonacci obj = new Fibonacci();
+            string actual = obj.getFibonacciNumbers(-5);
+            string expected = string.Empty;
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod()]
+        public void FibonacciWithZeroAsInput()
+        {
+            Fibonacci obj = new Fibonacci();
+            string actual = obj.getFibonacciNumbers(0);
+            string expected = string.Empty;
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod()]
+        public void FibonacciWithOneAsInput()
+        {
+            Fibonacci obj = new Fibonacci();
+            string actual = obj.getFibonacciNumbers(1);
+            string expected = "0";
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod()]
+        public void FibonacciWithLargeNumber()
+        {
+            Exception expectedExcetpion = null;
+            try
+            {
+                Fibonacci obj = new Fibonacci();
+                string actual = obj.getFibonacciNumbers(99999999);
+            }
+            catch (Exception ex)
+            {
+                expectedExcetpion = ex;
+            }
+
+            // Assert
+            Assert.IsNotNull(expectedExcetpion);
+
+        }
+    }
+}
